ea474c9c21546aff42932b32d57c519e3920eb2b
a.kurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-08-24 14:04:37 +0200

    move from old repo

---
+++ routes/product.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+
+const Product = require('../models/product');
+
+router.post('/', (req, res, next) => {
+  const product = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  product.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.get('/:id', (req, res, next) => {
+Product.findOne({
+    _id: req.params.id
+  }).then(
+    (product) => {
+      res.status(200).json(product);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.put('/:id', (req, res, next) => {
+  const product = new Product({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Product.updateOne({_id: req.params.id}, product).then(
+    () => {
+      res.status(201).json({
+        message: 'product updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.delete('/:id', (req, res, next) => {
+Product.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+router.get('/' +
+  '', (req, res, next) => {
+Product.find().then(
+    (products) => {
+      res.status(200).json(products);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+module.exports = router;
