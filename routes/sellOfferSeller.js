c91e19995fc05ca957561c36901f6a277bfe3246
akurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-10-07 14:15:46 +0200

    fix auth
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    fix auth
    
    squash

--- routes/sellOfferSeller.js
+++ routes/sellOfferSeller.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const auth = require("../config/auth");
 
 const sellOfferController = require('../controllers/sellOffer');
 
-router.get('/:id', sellOfferController.getAllSellOffersBySellerId);
+router.get('/:id', auth, sellOfferController.getAllSellOffersBySellerId);
 
 module.exports = router;
