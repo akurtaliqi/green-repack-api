326c908758575d25ddffd25d2b720f9d51c5c026
akurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-10-11 13:51:23 +0200

    fix stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,80 +1,11 @@
-const stripe = require('stripe')('sk_test_51JfOTNKqXtPaxbjmHLKF5pT44Yp0Yo3kf7fOLM42bv2wMwEdIgodPoeKUpbYv39IVUwjjrAQEmDmVc22aHb8MMAQ00qE7RQmhK');
+const express = require('express');
+const router = express.Router();
 
-const getPaymentIntent = async (req,res,next) => {
-    try {
-        const id = req.params.id;
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount:2000,
-            currency:'eur',
-            payment_method_types: ['card'],
-        })
-        res.send(paymentIntent);
-    } catch( error) {
-        res.status(400).send(error.message);
-    }
-};
+const stripe = require('../config/stripe');
 
-const createCheckoutSession = async(req,res,next) => {
-    try{
-        console.log("price id :" + req.query.id);
-        const session = await stripe.checkout.sessions.create({
+router.get('/', stripe.getPaymentIntent);
+router.post('/:id', stripe.createCheckoutSession);
+// router.post('/:id', stripe.createProduct);
+// router.get('/:id', stripe.createPrice);
 
-            shipping_address_collection: {
-                allowed_countries: ['FR'],
-              },
-            line_items: [
-            {
-                price: req.query.id,
-                quantity: 1
-                // quantity: req.query.quantity,
-            },
-            ],
-            payment_method_types: [
-            'card',
-            ],
-            mode: 'payment',
-            successURL: "http://localhost:8081/checkoutsuccess/"+this.$route.params.id,
-            cancelURL: 'http://localhost:8081/products/'+this.$route.params.id,
-        })
-        res.send(session);
-        res.redirect(303, session.url)
-    }catch(error){
-        res.status(400).send(error.message)
-    }
-
-};
-
-const createProduct = async(req,res,next) => {
-    try{
-        const product = await stripe.products.create({
-            name: req.query.name,
-            description:req.query.description,
-            images:[req.query.image]
-        });
-        res.send(product);
-    } catch(error) {
-        res.status(400).send(error.message)
-    }
-
-};
-
-const createPrice = async(req,res,next) => {
-    try {
-        const price = await stripe.prices.create({
-            product: req.query.id,
-            unit_amount: req.query.price,
-            currency: 'eur',
-        });
-        res.send(price);
-    }catch(error){
-        res.status(400).send(error.message)
-    }
-
-};
-
-export {
-    getPaymentIntent,
-    createProduct,
-    createCheckoutSession,
-    createPrice
-};
+module.exports = router;
