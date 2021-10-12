6c7fd4214401608292eb037523490e340d4d9a26
akurtaliqi <ajtene.kurtaliqi@gmail.com>
2021-10-12 08:44:00 +0200

    fix stripe

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const stripe = require('../config/stripe');
 
-router.get('/', stripe.getPaymentIntent);
-router.post('/:id', stripe.createCheckoutSession);
 // router.post('/:id', stripe.createProduct);
 // router.get('/:id', stripe.createPrice);
 
